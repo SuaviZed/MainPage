60d04501eaa43cef0719bc6c33857bf5a3818d14
ca-montenegro <ca.montenegro@uniandes.edu.co>
2018-06-11 19:54:20 -0500

    React commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                Made with ❤️ by Suavized
+            </div>
+        );
+    }
+}
+
+App.propTypes = {};
+
+export default App;
